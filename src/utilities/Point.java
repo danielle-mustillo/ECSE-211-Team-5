1fe602bbcdd3fc97a843e17ce7fe6c13b3459bef
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-06 15:38:28 -0500

    Point class was created. Please note the X and Y values and their orientation. Programming Guide Specifications contains a quick note on the orientation definitions.

--- src/utilities/Point.java
+++ src/utilities/Point.java
@@ -1,5 +1,35 @@
 package utilities;
 
 public class Point  {
+	private double x;
+	private double y;
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double angleTo(Point xy) {
+		double dx = this.getX() - xy.getX();
+		double dy = this.getY() - xy.getY();
+		return Math.tan(dx/dy);
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
 
 }
