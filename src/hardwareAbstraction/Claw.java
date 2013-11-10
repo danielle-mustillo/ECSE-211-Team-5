9fb7aa62e35caa0606800423ed316e51bdeddbf9
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-09 23:55:42 -0500

    Made protoype of Claw and Forklift. Simple methods. Must be adjusted

--- src/hardwareAbstraction/Claw.java
+++ src/hardwareAbstraction/Claw.java
@@ -1,5 +1,17 @@
 package hardwareAbstraction;
 
+import utilities.Settings;
+import lejos.nxt.remote.RemoteMotor;
+
 public class Claw {
+	static RemoteMotor claw = Settings.clawMotor;
+	static int value = 45; // 45 degrees. needs to be tested. 
+	
+	/**
+	 * This method grabs an object. Returns nothing
+	 */
+	public static void grabObject() {
+		claw.rotate(value);
+	}
 
 }
--- src/hardwareAbstraction/Forklift.java
+++ src/hardwareAbstraction/Forklift.java
@@ -1,5 +1,16 @@
 package hardwareAbstraction;
 
-public class Forklift {
+import lejos.nxt.remote.RemoteMotor;
+import utilities.Settings;
 
+public class Forklift {
+	static RemoteMotor lift = Settings.forkliftMotor;
+	static int value = 45; // 45 degrees. needs to be tested. 
+	
+	/**
+	 * This method lifts an object. Returns nothing
+	 */
+	public static void liftObject() {
+		lift.rotate(value);
+	}
 }
