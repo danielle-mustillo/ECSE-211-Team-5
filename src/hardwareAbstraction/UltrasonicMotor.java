af45e56805cf4653977bd560b9a396277a08232e
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-14 20:17:36 -0500

    Began an implementation of a scanning feature for the robot. The robot side ultrasonic sensors should be able to move back and forward now.

--- src/hardwareAbstraction/UltrasonicMotor.java
+++ src/hardwareAbstraction/UltrasonicMotor.java
@@ -1,5 +1,26 @@
 package hardwareAbstraction;
 
+import utilities.Settings;
+
 public class UltrasonicMotor {
+	/**
+	 * The default position is set to be with the two side ultrasonic sensors
+	 * pointing away from the center of the robot. Therefore, it will be at an
+	 * angle when viewed from the front of the robot.
+	 */
+	public static void setDefaultPosition() {
+		Settings.ultrasonicMotor.setSpeed(200);
+		Settings.ultrasonicMotor.rotate(45);
+	}
+	
+	/**
+	 * The forward position is set to be with the two side ultrasonic sensors
+	 * pointing directly ahead, in line with the centre ultrasonic sensor. Therefore, it will be at an
+	 * angle when viewed from the front of the robot.
+	 */
+	public static void setForwardPosition() {
+		Settings.ultrasonicMotor.setSpeed(200);
+		Settings.ultrasonicMotor.rotate(-45);
+	}
 
 }
