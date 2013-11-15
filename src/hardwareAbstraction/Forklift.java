94ec2b4fa63a4f3f6f49efff9aa94e3448536b94
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-14 20:22:45 -0500

    The forklift will now be used to detect obstacles and blue blocks. The method to do this is that it will go to a default height and detect the objects in the range and compare that with the values at its default height. The difference of these two will uniquely identify the obstacle. This has all been documented elsewhere. The work done sofar on this forklift is minimal just to allow this to happen. Also boolean identifiers of the forklift current state was added for convenience.

--- src/hardwareAbstraction/Forklift.java
+++ src/hardwareAbstraction/Forklift.java
@@ -6,8 +6,11 @@ import utilities.Settings;
 
 public class Forklift {
 	static RemoteMotor lift = Settings.forkliftMotor;
-	static int distance = -15; // 15 cm upwards. needs to be tested. 
+	static int liftHeight = -15; // 15 cm upwards. Should be ok
+	static int scanHeight = -10; // 10 cm upwards. Needs to be tested. 
 	private static double radius = 1; //radius of "spool". Must be tested. 
+	public static boolean atScanHeight = false;
+	public static boolean atLiftHeight = false;
 	
 	/**
 	 * This method lifts an object. Returns nothing.
@@ -15,9 +18,10 @@ public class Forklift {
 	 */
 	public static void liftObject() {
 		RConsole.println("lifting object");
+		atLiftHeight = true;
 		try {
 			lift.setSpeed(100);
-			lift.rotate(convertDistanceToAngle(distance));
+			lift.rotate(convertDistanceToAngle(liftHeight));
 		} catch (ArrayIndexOutOfBoundsException e){
 		
 		}
@@ -29,13 +33,46 @@ public class Forklift {
 	 */
 	public static void lowerObject() {
 		RConsole.println("lowering object");
+		atLiftHeight = false;
 		try {
 			lift.setSpeed(100);
-			lift.rotate(-convertDistanceToAngle(distance));
+			lift.rotate(-convertDistanceToAngle(liftHeight));
 		} catch (ArrayIndexOutOfBoundsException e){
 		
 		}
 	}
+	
+	/**
+	 * This method will raise the forklift to allow the color sensor to identify the block. 
+	 * @bug the execution of external motors causes exceptions. Try-catch block was put for now. Must be fixed.
+	 */
+	public static void setScanHeight() {
+		RConsole.println("lifting to scan height");
+		atScanHeight = true;
+		try {
+			lift.setSpeed(100);
+			lift.rotate(convertDistanceToAngle(scanHeight));
+		} catch (ArrayIndexOutOfBoundsException e){
+		
+		}
+	}
+	
+	/**
+	 * This method will lower the forklift after identifying the block. 
+	 * @bug the execution of external motors causes exceptions. Try-catch block was put for now. Must be fixed.
+	 */
+	public static void resetScanHeight() {
+		RConsole.println("lowering to default height");
+		atLiftHeight = false;
+		try {
+			lift.setSpeed(100);
+			lift.rotate(-convertDistanceToAngle(scanHeight));
+		} catch (ArrayIndexOutOfBoundsException e){
+		
+		}
+	}
+	
+	
 
 	/**
 	 * This method turns a distance into an angle for the robot to turn. Takes as parameter the distance you want to lift. 
