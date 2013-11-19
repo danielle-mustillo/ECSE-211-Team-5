f3bec86902bd8479f1caa3375e9f5be65715600e
danielle-mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-19 18:25:57 -0500

    Few bug fixes for navigation.

--- src/services/Navigation.java
+++ src/services/Navigation.java
@@ -14,8 +14,8 @@ import manager.*;
 
 /**
  * 
- * Navigates the robot along a route, which is contained in this class
- * the route is set by the search and drop off controllers.
+ * Navigates the robot along a route, which is contained in this class the route
+ * is set by the search and drop off controllers.
  * 
  * Further work required with scanAhead()
  * 
@@ -57,7 +57,7 @@ public class Navigation implements TimerListener {
 				|| manager.cm.getState() == State.DROP_OFF
 				|| manager.cm.getState() == State.RECOGNIZE
 				|| manager.cm.getState() == State.TESTING) {
-
+			
 			if (route.empty()) {
 				// nothing is done
 			} else {
@@ -83,50 +83,55 @@ public class Navigation implements TimerListener {
 					if (!scannedAhead) {
 						manager.hm.drive.stop();
 						this.pause();
-						UltrasonicMotor.setForwardPosition();
+//						UltrasonicMotor.setForwardPosition();
 						try {
 							Thread.sleep(manager.hm.ultrasonicPoller.pollRate * 6);
 						} catch (InterruptedException e) {
 						}
-						UltrasonicMotor.setDefaultPosition();
+
 						Sound.beep();
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e) {
-						}
 						int lowest = manager.hm.ultrasonicPoller
 								.getLowestReading();
 						Sound.beep();
-						if (lowest < 20)
-							manager.cm.setState(State.RECOGNIZE);
-						else if (lowest < 30)
-							route.push(manager.sm.odo.getPosition().addDistanceToPosition(lowest - 5));
-						Sound.beep();
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+
+//						UltrasonicMotor.setDefaultPosition();
 						this.start();
-						Sound.beep();
+						Sound.twoBeeps();
+						if (lowest < 20) {}
+//							manager.cm.setState(State.RECOGNIZE);
+						else if (lowest < 30)
+							route.push(manager.sm.odo.getPosition()
+									.addDistanceToPosition(lowest - 5));
 						scannedAhead = true;
-						travelTo();
-					} else {
-						// stop the motors, reset scanning state and get next
-						// destination.
-						manager.hm.drive.stop();
-						scannedAhead = false;
-						route.pop();
 					}
+				} else {
+					// stop the motors, reset scanning state and get next
+					// destination.
+					manager.hm.drive.stop();
+					scannedAhead = false;
+					route.pop();
 				}
+
 			}
 		}
 	}
-	
+
 	public void pause() {
 		this.time.stop();
+		this.time = null;
 	}
-	
+
 	public void start() {
 		RConsole.println("Navigation started");
+		this.time = new Timer(UPDATE_PERIOD, this);
 		this.time.start();
 	}
-	
+
 	private void setupDeltaPositonAndHeading() {
 		currentPos = manager.sm.odo.getPosition();
 
@@ -136,11 +141,12 @@ public class Navigation implements TimerListener {
 
 		// the required theta to travel to our destination
 		double theta = Math.atan2(dY, dX);
-		
+
 		// adjust to [0, 2PI]
 		theta = Angle.principleAngle(theta);
-		
-		// set dH to the difference of theta and currentTheta adjust to [-PI, PI]
+
+		// set dH to the difference of theta and currentTheta adjust to [-PI,
+		// PI]
 		dH = Angle.minimumAngle(currentPos.theta, theta);
 	}
 
@@ -152,7 +158,7 @@ public class Navigation implements TimerListener {
 
 		// Distance to destination
 		double distanceToTravel = (dX * Math.cos(currentPos.theta))
-								+ (dY * Math.sin(currentPos.theta));
+				+ (dY * Math.sin(currentPos.theta));
 		// high speed
 		if (distanceToTravel > 3) {
 			manager.hm.drive.setSpeeds(MAX_FORWARD_SPEED, dL);
@@ -174,65 +180,71 @@ public class Navigation implements TimerListener {
 			manager.hm.drive.stop();
 		}
 	}
-	
-	
+
 	public void turnToComplete(double angle) {
-		
+
 		angle = Angle.principleAngle(angle);
-		// set dH to the difference of theta and currentTheta adjust to [-PI, PI]
+		// set dH to the difference of theta and currentTheta adjust to [-PI,
+		// PI]
 		double dH = Angle.minimumAngle(currentPos.theta, angle);
-		
-		while(Math.abs(dH) > 0.01) {
+
+		while (Math.abs(dH) > 0.01) {
 			dH = Angle.minimumAngle(currentPos.theta, angle);
 			turnTo(dH);
 			manager.um.nap(70);
 		}
-		
+
 		manager.hm.drive.stop();
-		
+
 	}
-	
+
 	public int calculateRotationSpeed(double dH) {
 		if (Math.abs(dH) > 0.01) {
 			// if error positive and greater than 0.2 rad -> max speed CCW
 			if (dH > 0.3) {
 				return -MAX_ROTATE_SPEED;
 			}
-			// positive error, between .05 and .15 , so turn slow CCW to prevent overshoot
+			// positive error, between .05 and .15 , so turn slow CCW to prevent
+			// overshoot
 			else if (dH > .15) {
 				return -MAX_ROTATE_SPEED / 2;
 			}
-			// positive error, between .05 and .15 , so turn slow CCW to prevent overshoot
+			// positive error, between .05 and .15 , so turn slow CCW to prevent
+			// overshoot
 			else if (dH > .05) {
 				return -MAX_ROTATE_SPEED / 6;
 			}
-			// positive error, but close to 0, so turn slow CCW to prevent overshoot
+			// positive error, but close to 0, so turn slow CCW to prevent
+			// overshoot
 			else if (dH > 0.01) {
 				return -MAX_ROTATE_SPEED / 16;
 			}
-			
+
 			// if error negative and greater than 0.2 rad -> max speed CW
 			if (dH < -0.3) {
 				return MAX_ROTATE_SPEED;
 			}
-			// negative error, between .05 and .15 , so turn slow CW to prevent overshoot
+			// negative error, between .05 and .15 , so turn slow CW to prevent
+			// overshoot
 			else if (dH < -.15) {
 				return MAX_ROTATE_SPEED / 2;
 			}
-			// negative error, between .05 and .15 , so turn slow CW to prevent overshoot
+			// negative error, between .05 and .15 , so turn slow CW to prevent
+			// overshoot
 			else if (dH < -.05) {
 				return MAX_ROTATE_SPEED / 6;
 			}
-			// negative error, but close to 0, so turn slow CW to prevent overshoot
+			// negative error, but close to 0, so turn slow CW to prevent
+			// overshoot
 			else if (dH < -0.01) {
 				return MAX_ROTATE_SPEED / 16;
 			}
 		}
-		//if this point is reached, dH is basically zero (<0.6deg)
+		// if this point is reached, dH is basically zero (<0.6deg)
 		return 0;
 	}
 
-	//accessors and mutators
+	// accessors and mutators
 	public Stack<Point> getRoute() {
 		return route;
 	}
@@ -240,10 +252,11 @@ public class Navigation implements TimerListener {
 	public void setRoute(Stack<Point> route) {
 		this.route = route;
 	}
-	
-	
+
 	/**
-	 * This method will export the route currently programmed and resets it to an empty route. 
+	 * This method will export the route currently programmed and resets it to
+	 * an empty route.
+	 * 
 	 * @return
 	 */
 	public Stack<Point> exportAndResetRoute() {
@@ -251,7 +264,7 @@ public class Navigation implements TimerListener {
 		this.route = new Stack<Point>();
 		return export;
 	}
-	
+
 	public void addToRoute(Point xy) {
 		this.scannedAhead = false;
 		this.route.push(xy);
