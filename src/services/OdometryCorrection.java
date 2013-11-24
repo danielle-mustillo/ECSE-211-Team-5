682382eb0083ae06ce7d28305309fa249ee8430b
RileyvanRyswyk <rileyvan@gmail.com>
2013-11-24 18:17:07 -0500

    Odometer and OdometryCorrection commented

--- src/services/OdometryCorrection.java
+++ src/services/OdometryCorrection.java
@@ -8,25 +8,74 @@ import lejos.nxt.comm.RConsole;
 import lejos.util.Timer;
 import lejos.util.TimerListener;
 import manager.*;
-
+/**
+ * OdometryCorrection makes use of the {@link LinePoller},
+ * each time a new line is detected, it will record the position of the odometer and what sensor detected the line.
+ * Then it will check to see if the previous detected line was with the other sensor.  If that is the case then, provided
+ * the difference in x, y, theta of the two poistions is relatively small and the robot is roughly in the center of the tile, 
+ * it will update x or y and theta of the {@link odometer}.
+ * 
+ * Note: The current methods used required the robot to travel in the center of the tiles, frequently enough.
+ * @author Riley 
+ *
+ */
 public class OdometryCorrection implements TimerListener  {
-	
+	/**
+	 * Period to update, should be similar to update period of {@link LinePoller}
+	 */
 	public final int UPDATE_PERIOD = 15;
+	/**
+	 * Max distance to adjust x or y
+	 */
 	public final int MAX_CHANGE_DISTANCE = 8;
+	/**
+	 * Max angle to adjust theta;
+	 */
 	public final int MAX_CHANGE_ANGLE = 30;
+	/**
+	 * Max distance from the center of the tile to still correct
+	 */
+	public final int MAX_DISTANCE_FROM_TILE_CENTER = 3;
+	/**
+	 * Max distance between points to correct (i.e. max distance between left sensor detection position and right senosr detection position)
+	 */
+	public final int MAX_DISTANCE_BETWEEN_POINTS = 5;
 	
+	/**
+	 * Timer for timer listener
+	 */
 	public Timer timer;
+	/**
+	 * {@link Manager} for access to robots other functions 
+	 */
 	public Manager manager;
-	
+	/**
+	 * id of the left sensor
+	 */
 	private int left = 0;
+	/**
+	 * id of the right sensor
+	 */
 	private int right = 1;
 	
+	/**
+	 * The odometer position at the detection of the last line
+	 */
 	private Position lastPos;
+	/**
+	 * the id of the last sensor to detect a line
+	 */
 	private int lastSensor;
 	
-	//Sets (0,0) to center of the of the first grid intersection
+	/**
+	 * For offseting the grid lines (i.e. to move 0,0 away from the first grid intersection )
+	 */
 	private double xOffset = 0, yOffset = 0;
 	
+	/**
+	 * Initializes timer
+	 * @param manager
+	 */
 	public OdometryCorrection(Manager manager) {
 		this.timer = new Timer(UPDATE_PERIOD, this);
 		this.lastPos = null;
@@ -34,20 +83,26 @@ public class OdometryCorrection implements TimerListener  {
 		this.manager = manager;
 	}
 	
+	/**
+	 * Starts odometry Correction
+	 */
 	public void start() {
 		timer.start();
 	}
 	
+	/**
+	 * Stops odometry Correction
+	 */
 	public void stop() {
 		timer.stop();
 	}
 	
-	
 	/**
-	 * Based on the which sensor is entered a line and the state of the last detected one, 
-	 * the method will call the relevant lineDetected or setup lastPos
+	 * Based on which sensor has entered a line and the state of the last detected one, 
+	 * the method will call the relevant lineDetected and/or setup lastPos
 	 */
 	public void timedOut() {
+		//Line detected by left sensor
 		if(manager.hm.linePoller.enteringLine(left)) {
 			if(this.lastPos == null) {
 				this.lastPos = manager.sm.odo.getPosition();
@@ -61,7 +116,7 @@ public class OdometryCorrection implements TimerListener  {
 				lastSensor = left;
 			}
 		}
-		
+		//Line detected by right sensor
 		if(manager.hm.linePoller.enteringLine(right)) {
 			if(this.lastPos == null) {
 				this.lastPos = manager.sm.odo.getPosition();
@@ -80,7 +135,7 @@ public class OdometryCorrection implements TimerListener  {
 	}
 	
 	/**
-	 * When only one sensor detected a line
+	 * When only one sensor detected a line. Currently not implemented. 
 	 * @param pos
 	 * @param sensor
 	 */
@@ -90,7 +145,7 @@ public class OdometryCorrection implements TimerListener  {
 	
 	
 	/**
-	 * Two sensors detected a line
+	 * Both sensors detected a line, decides whether the points are consider value according to thresholds, and then how to correct the robots position
 	 * @param leftPos
 	 * @param rightPos
 	 */
@@ -99,12 +154,14 @@ public class OdometryCorrection implements TimerListener  {
 		/*
 		 * Ensures that the point are reasonably close to each other
 		 */
-		if(leftPos.distanceToPoint(rightPos) > 5 && Math.abs(leftPos.theta-rightPos.theta) > 0.1 ) {
+		if(leftPos.distanceToPoint(rightPos) > MAX_DISTANCE_BETWEEN_POINTS && Math.abs(leftPos.theta-rightPos.theta) > 0.1 ) {
 			return;
 		}
 		
+		//Average the x and y of the line detected, as the robot should detect both at the same time if it is going perpendicular to the grid line
 		double x = (leftPos.x + rightPos.x) / 2.0;
 		double y = (leftPos.y + rightPos.y) / 2.0;
+		//Average the heading 
 		double theta = (leftPos.theta + rightPos.theta) / 2.0;
 		
 		double dx = 0;
@@ -122,14 +179,14 @@ public class OdometryCorrection implements TimerListener  {
 			double line = getClosestX(x, theta);
 			
 			//make sure the sensor is not close to a line parallel to direction of travel
-			if(Math.abs(x-line) > Settings.LS_WIDTH / 2 + 3) {
+			if(Math.abs(x-line) > Settings.LS_WIDTH / 2 + MAX_DISTANCE_FROM_TILE_CENTER) {
 				
 				/*
 				 * getClosestY returns the closest line plus the offset for the light Sensor	
 				 */
 				line = getClosestY(y, theta);
 				
-				//if we are within 10cm of the line, update our position
+				//if we are within MAX_CHANGE_DISTANCE of the line, set dy
 				if(Math.abs(line - y) < MAX_CHANGE_DISTANCE) {
 					RConsole.println("dY: " + String.valueOf(line-y));
 					dy = line - y;
@@ -149,14 +206,14 @@ public class OdometryCorrection implements TimerListener  {
 			double line = getClosestY(y, theta);
 			
 			// make sure the sensor is not close to a line parallel with direction of travel
-			if(Math.abs(y-line) > Settings.LS_WIDTH / 2 + 3) {
+			if(Math.abs(y-line) > Settings.LS_WIDTH / 2 + MAX_DISTANCE_FROM_TILE_CENTER) {
 			
 				/*
 				 * We have already detected a x line, so get the closest line
 				 */
 				line = getClosestX(x, theta);
 				
-				//if we are within 10cm of the line, update our position
+				//if we are within MAX_CHANGE_DISTANCE of the line, update dx
 				if(Math.abs(line - x) < MAX_CHANGE_DISTANCE) {
 					RConsole.println("dX: " + String.valueOf(line-x));
 					dx = line - x;
@@ -170,8 +227,9 @@ public class OdometryCorrection implements TimerListener  {
 		/* 
 		 * Angle Correction
 		 */
-		
+		//correction angle is arctan (displacement/width)
 		double dTheta = Math.atan(leftPos.distanceToPoint(rightPos) / Settings.LS_WIDTH);
+		//adjust sign
 		if(firstSensor == left) {
 			dTheta *= -1;
 		}
