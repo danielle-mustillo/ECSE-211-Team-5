739ba4696816aac57cc9a6bb83a533eff479d52d
Danielle Mustillo <danielle.mustillo@mail.mcgill.ca>
2013-11-26 00:51:13 -0500

    Fixed the new method in Position. Added the JavaDocs to it.

--- src/utilities/Position.java
+++ src/utilities/Position.java
@@ -1,10 +1,11 @@
 package utilities;
 
 /**
- * Using the {@link Point} class as a base, Position adds an angle theta.  
+ * Using the {@link Point} class as a base, Position adds an angle theta.
+ * 
  * @author Riley
  * @author Danielle
- *
+ * 
  */
 public class Position extends Point {
 
@@ -12,6 +13,7 @@ public class Position extends Point {
 
 	/**
 	 * Initializes the Position as per the passed x, y, theta
+	 * 
 	 * @param x
 	 * @param y
 	 * @param theta
@@ -21,6 +23,7 @@ public class Position extends Point {
 		this.y = y;
 		this.theta = theta;
 	}
+
 	/**
 	 * Initializes an empty Position object
 	 */
@@ -38,13 +41,15 @@ public class Position extends Point {
 	}
 
 	/**
-	 * This method assumes the robot is at this position presently. It adds a distance to
-	 * the position, generating a new {@link Point} object. This is useful when the robot needs to
-	 * travel somewhere in a straight line ahead of it. Formula used for newX
-	 * position is x + deltaX where deltaX is distance * cos(theta). Theta is
-	 * discarded due to logical reasons (the angle the robot ends up at its
-	 * destination is not necessarily the same as the angle it started with.
+	 * This method assumes the robot is at this position presently. It adds a
+	 * distance to this position, generating a new {@link Point} object. This is
+	 * useful when the robot needs to travel somewhere in a straight line ahead
+	 * of it. Formula used for newX position is x + deltaX where deltaX is
+	 * distance * cos(theta). A Point object is returned due to logical reasons
+	 * (the angle the robot ends up at its destination is not necessarily the
+	 * same as the angle it started with).
 	 * <p>
+	 * 
 	 * @param distance
 	 *            A distance the robot should travel forward
 	 * @return A point with the destination coordinates.
@@ -54,10 +59,29 @@ public class Position extends Point {
 		double posY = this.y + distance * Math.sin(this.theta);
 		return new Point(posX, posY);
 	}
-	
+
+	/**
+	 * This method assumes the robot is at this position presently. It adds a
+	 * distance to this position, generating a new {@link Point} object. It also
+	 * adds the input angle to that position. This method is useful when the
+	 * robot needs to travel somewhere relative to its current position. Formula
+	 * used for newX position is x + deltaX where deltaX is distance *
+	 * cos(theta). Theta in this case is the sum of its current angle and the
+	 * input angle A Point object is returned due to logical reasons (the angle
+	 * the robot ends up at its destination is not necessarily the same as the
+	 * angle it started with).
+	 * <p>
+	 * 
+	 * @param distance
+	 *            A distance the robot should travel
+	 * @param angle An angle the robot wants to turn by (relative to its current angle). 
+	 * @return A point with the destination coordinates.
+	 */
 	public Point addDisAndAngleToPosition(double distance, double angle) {
-		double posX = this.x + distance * Math.cos(Angle.principleAngle(angle - this.theta));
-		double posY = this.y + distance * Math.cos(Angle.principleAngle(angle - this.theta));
+		double posX = this.x + distance
+				* Math.cos(Angle.principleAngle(angle + this.theta));
+		double posY = this.y + distance
+				* Math.sin(Angle.principleAngle(angle + this.theta));
 		return new Point(posX, posY);
 	}
 }
